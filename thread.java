a96707971691a2350fa45f8417d41dae0cbca7ac
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-08-09 23:14:31 +0530

    Create thread.java

---
+++ thread.java
@@ -0,0 +1,12 @@
+class A
+  {
+    public static void main(String arg[])
+    {
+      String name=Thread.currentThread().getName();
+      System.out.println(name);
+    }
+  }
+
+
+#output
+  --main
