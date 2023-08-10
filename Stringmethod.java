25579dd2cb94686900e0e58ef23dff2d8bbf8619
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-08-10 22:32:50 +0530

    Create Stringmethod.java

---
+++ Stringmethod.java
@@ -0,0 +1,9 @@
+class Test
+  {
+    public static void main(String arg[])
+    {
+      String s="sachin";
+      System.out.println(s.toUpperCase());
+      System.out.println(s.toLowerCase());
+    }
+  }
