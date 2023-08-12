bb2b9a683c0f29615f7a7b6b966fed30710f891f
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-08-12 18:03:25 +0530

    Update String.java

--- String.java
+++ String.java
@@ -1,1 +1,10 @@
-
+import java.util.*;
+class Test
+  {
+    public static void main(String arg[])
+    {
+      Scanner sc = new Scanner(System.in);
+      String s = sc.nextLine();
+      System.out.println(s);
+    }
+  }
