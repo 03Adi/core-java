db0da081a18c5695c4895b3179f7ffe98e91972e
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-08-16 12:16:54 +0530

    Create StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,27 @@
+Public class Test
+  {
+    public static void main(String arg[])
+    {
+      //reverse method
+      StringBuilder str = new StringBuilder("Aditya");
+      str.reverse();
+      System.out.println(str);
+
+      //replace method
+      Stg.replace(0,1,"hi");
+      System.out.println(stg);
+
+     //delete method
+      str.delete(1,3);
+      System.out.println(str);
+
+      //append method
+      str.append("hello");
+      System.out.println(str);
+
+      //insert method
+      str.insert(3,"hi");
+      System.out.println(str);
+      
+    }
+  }
