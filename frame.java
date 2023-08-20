e42e86b017dc7e57cf5696c2c88e9a2ed8e8634a
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-08-20 14:35:54 +0530

    Create frame.java

---
+++ frame.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+class myframe extends Frame
+  {
+    public static void main(String arg[])
+    {
+      Frame f=new Frame();
+      f.setSize(300,400);
+      F.setSize(true);
+    }
+  }
