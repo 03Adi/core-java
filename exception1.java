0b69a88e8e81555f3b925532db3ad7cf40531b0c
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-09-14 10:43:55 +0530

    Create exception1.java

---
+++ exception1.java
@@ -0,0 +1,15 @@
+//unchecked exception
+class exception
+  {
+    public static void main(String[] args)
+    {
+      try
+        {
+          int x=10/0;
+        }
+      catch(ArithmeticException e)
+        {
+          System.ou.println(e);
+        }
+    }
+  }
