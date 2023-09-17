c744ebaa559c4d7755cfc4b3a7e64718677f0662
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-09-17 20:05:53 +0530

    Create encapsulationexample.java

---
+++ encapsulationexample.java
@@ -0,0 +1,16 @@
+//Encapsulation=data hiding + Abstraction
+public class Account
+  {
+    private double balance;
+    public double getBalane();//data hiding
+    {
+      //valiadtion
+    return balance;
+    }
+    public void setBalanc(double);//data abstraction
+    {
+      //valiadte
+    this.balance=balance;
+    }
+  }
+}
