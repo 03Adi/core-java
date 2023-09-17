f07caa2d5f73c674d0659d9628cea63d86f4b935
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-09-17 19:58:57 +0530

    Create datahiding.java

---
+++ datahiding.java
@@ -0,0 +1,13 @@
+//data hhiding by private method class
+public class account
+  {
+    private double balance;
+    ..
+...
+public double getBalance();
+    {
+      //validation
+      return balance;
+    }
+  }
+    //After validation and identification outside person are access internal data
