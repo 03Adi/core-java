48db131e023714c65bba173861cf0c0e0d7cc9d2
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-09-19 09:45:38 +0530

    Create priorityoverloading.java

---
+++ priorityoverloading.java
@@ -0,0 +1,13 @@
+import java.*.io;
+class p
+  {
+    public void m1() throws IOException
+    {
+    }
+  }
+class c extends p
+  {
+    public void m1() throws EOException,InterrruptException
+    {
+    }
+  }
