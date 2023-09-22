7e6772172aa018634e4576d5c6d940c8c6e687ab
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2023-09-22 10:04:30 +0530

    Create collection10.java

---
+++ collection10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class collection10 {
+    public static void main(String[] args) {
+     ArrayList<String> al=new ArrayList<String>();
+     System.out.println("Is ArrayList Empty: "+al.isEmpty());
+        al.add("Ravi");
+        al.add("Vijay");
+        al.add("Ajay");
+        System.out.println("After Insertion");
+        System.out.println("Is ArrayList Empty: "+al.isEmpty());
+    }
+    
+}
